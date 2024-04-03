a46f32c30fdffbb36754fe55fc9e7dbc8d341552
Michael Telahun <michael.telahun@dindin.et>
2024-04-03 20:18:52 +0300

    test: PlaceOrder command with food item should trigger a FoodOrderPlaced event

--- src/domain/tab/aggregate.rs
+++ src/domain/tab/aggregate.rs
@@ -157,11 +157,10 @@ pub mod tests {
         let event = event.pop().unwrap();
         assert_eq!(
             event,
-            TabEvent::OrderPlaced {
-                order_item: OrderItem {
+            TabEvent::FoodOrderPlaced {
+                order_item: FoodOrder {
                     menu_number: 1,
                     description: "Steak".into(),
-                    is_drink: false,
                     price: Decimal::from(10),
                 }
             },
@@ -169,43 +168,82 @@ pub mod tests {
         );
     }
 
-    #[test]
-    #[allow(non_snake_case)]
-    fn given_opened_tab_when_order_1_drink_then_ItemOrdered_event() {
-        // Arrange
-        let waiter_id = WaiterId::new();
-        let tab_services = TabServices {};
-        let order_item = OrderItem {
-            menu_number: 2,
-            description: "Coca-Cola".into(),
-            is_drink: true,
-            price: Decimal::from(3),
-        };
-        let executor = TestFramework::<Tab>::with(tab_services).given(vec![TabEvent::TabOpened {
-            waiter_id,
-            table: 1,
-        }]);
+    // #[test]
+    // #[allow(non_snake_case)]
+    // fn given_opened_tab_when_order_1_drink_then_ItemOrdered_event() {
+    //     // Arrange
+    //     let waiter_id = WaiterId::new();
+    //     let tab_services = TabServices {};
+    //     let order_item = OrderItem {
+    //         menu_number: 2,
+    //         description: "Coca-Cola".into(),
+    //         is_drink: true,
+    //         price: Decimal::from(3),
+    //     };
+    //     let executor = TestFramework::<Tab>::with(tab_services).given(vec![TabEvent::TabOpened {
+    //         waiter_id,
+    //         table: 1,
+    //     }]);
 
-        // Act
-        let mut event = executor
-            .when(TabCommand::PlaceOrder { order_item })
-            .inspect_result()
-            .expect("failed to execute command: OrderItem");
+    //     // Act
+    //     let mut event = executor
+    //         .when(TabCommand::PlaceOrder { order_item })
+    //         .inspect_result()
+    //         .expect("failed to execute command: OrderItem");
 
-        // Assert
-        assert_eq!(event.len(), 1);
-        let event = event.pop().unwrap();
-        assert_eq!(
-            event,
-            TabEvent::OrderPlaced {
-                order_item: OrderItem {
-                    menu_number: 2,
-                    description: "Coca-Cola".into(),
-                    is_drink: true,
-                    price: Decimal::from(3),
-                }
-            },
-            "ItemOrdered"
-        );
-    }
+    //     // Assert
+    //     assert_eq!(event.len(), 1);
+    //     let event = event.pop().unwrap();
+    //     assert_eq!(
+    //         event,
+    //         TabEvent::DrinkOrderPlaced {
+    //             order_item: DrinkItem {
+    //                 menu_number: 2,
+    //                 description: "Coca-Cola".into(),
+    //                 price: Decimal::from(3),
+    //             }
+    //         },
+    //         "ItemOrdered"
+    //     );
+    // }
+
+    // #[test]
+    // #[allow(non_snake_case)]
+    // fn given_opened_tab_when_order_multiple_items_then_multiple_OrderPlaced_events() {
+    //     // Arrange
+    //     let waiter_id = WaiterId::new();
+    //     let tab_services = TabServices {};
+    //     let order_item = OrderItem {
+    //         menu_number: 2,
+    //         description: "Coca-Cola".into(),
+    //         is_drink: true,
+    //         price: Decimal::from(3),
+    //     };
+    //     let executor = TestFramework::<Tab>::with(tab_services).given(vec![TabEvent::TabOpened {
+    //         waiter_id,
+    //         table: 1,
+    //     }]);
+
+    //     // Act
+    //     let mut event = executor
+    //         .when(TabCommand::PlaceOrder { order_item })
+    //         .inspect_result()
+    //         .expect("failed to execute command: OrderItem");
+
+    //     // Assert
+    //     assert_eq!(event.len(), 1);
+    //     let event = event.pop().unwrap();
+    //     assert_eq!(
+    //         event,
+    //         TabEvent::OrderPlaced {
+    //             order_item: OrderItem {
+    //                 menu_number: 2,
+    //                 description: "Coca-Cola".into(),
+    //                 is_drink: true,
+    //                 price: Decimal::from(3),
+    //             }
+    //         },
+    //         "ItemOrdered"
+    //     );
+    // }
 }
