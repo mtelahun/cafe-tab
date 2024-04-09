9d3fb396ac5a2445773d83db2247efc57ffb7d3f
Michael Telahun Makonnen <michael.telahun@dindin.et>
2024-04-09 16:16:17 +0300

    test: refactor (#3)

---
+++ tests/test_state.rs
@@ -0,0 +1,92 @@
+use cafe_tab::{
+    domain::tab::{
+        command::TabCommand, queries::kitchen::KitchenTodoList, services::TabServices,
+        tab_id::TabId, waiter_id::WaiterId,
+    },
+    infrasctructure::{
+        persistence::context::{
+            connection_parameters::ConnectionBuilder,
+            postgres::{create_db, migrate_db, postgres_pool},
+        },
+        respository::postgresql::cqrs::{cqrs_tab, TabCqrsFramework},
+    },
+    shared_kernel::KitchenTabViewRepository,
+};
+use secrecy::Secret;
+use uuid::Uuid;
+
+pub struct TestState {
+    pub tab_id: TabId,
+    pub tab_aggregate: TabCqrsFramework,
+    pub tab_kitchen_todo_list: KitchenTabViewRepository,
+}
+
+#[derive(Debug)]
+pub enum AggregateState {
+    Open,
+    None,
+}
+
+impl TestState {
+    pub async fn new(aggregate_state: AggregateState) -> Self {
+        let db_name = Uuid::new_v4().to_string();
+        let params = ConnectionBuilder::new()
+            .with_credentials("postgres", Secret::new(String::from("password")))
+            .with_database(&db_name)
+            .build();
+        create_db(&params).await;
+        migrate_db(&params).await;
+        let pool = postgres_pool(&params).await;
+        let services = TabServices {};
+        let tab_kitchen_todo_list = KitchenTabViewRepository::new(pool.clone());
+        let tab_aggregate = cqrs_tab(pool, services, tab_kitchen_todo_list.clone());
+        let tab_id = TabId::new();
+        let waiter_id = WaiterId::new();
+        Self::initialize_aggregate_state(&tab_aggregate, tab_id, waiter_id, aggregate_state).await;
+
+        Self {
+            tab_id,
+            tab_kitchen_todo_list,
+            tab_aggregate,
+        }
+    }
+
+    pub async fn execute_command(&self, command: TabCommand) {
+        self.tab_aggregate
+            .execute(&self.tab_id.to_string(), command)
+            .await
+            .expect("failed to order execute a command on the aggregate");
+    }
+
+    pub async fn load_kitchen_todo_list(&self) -> KitchenTodoList {
+        self.tab_kitchen_todo_list
+            .load(&self.tab_id.to_string())
+            .await
+            .expect("failed to load the kitchen tab view")
+            .unwrap()
+    }
+
+    async fn initialize_aggregate_state(
+        tab_aggregate: &TabCqrsFramework,
+        tab_id: TabId,
+        waiter_id: WaiterId,
+        aggregate_state: AggregateState,
+    ) {
+        match aggregate_state {
+            AggregateState::Open => {
+                tab_aggregate
+                    .execute(
+                        &tab_id.to_string(),
+                        TabCommand::OpenTab {
+                            id: tab_id,
+                            waiter_id,
+                            table: 1,
+                        },
+                    )
+                    .await
+                    .expect("failed to open tab");
+            }
+            AggregateState::None => {}
+        }
+    }
+}
