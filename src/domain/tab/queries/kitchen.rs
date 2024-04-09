8835a2bdfd6fed1b89802f8f237e93902762e09f
Michael Telahun <michael.telahun@dindin.et>
2024-04-09 14:53:42 +0300

    Update read model for kitchen (Chef) to get list of all food orders by tab
    - the proper abstraction is the KitchenTodoList and not KitchenTodoGroup
    - KitchenTodoList is a list of structures containing the Tab ID and its food orders
    - not sure how to handle locking for now

--- src/domain/tab/queries/kitchen.rs
+++ src/domain/tab/queries/kitchen.rs
@@ -1,46 +1,43 @@
 use async_trait::async_trait;
 use cqrs_es::View;
 use serde::{Deserialize, Serialize};
-use tokio::sync::RwLock;
 
 use crate::domain::tab::{aggregate::Tab, tab_id::TabId};
 
 #[async_trait]
 pub trait KitchenTodoListQuery: Sized {
-    async fn get_kitchen_todo_list(&self) -> Vec<KitchenTabView>;
+    async fn get_kitchen_todo_list(&self) -> Vec<TodoListGroup>;
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Deserialize, Serialize)]
 pub struct KitchenTodoList {
-    inner: RwLock<Vec<KitchenTabView>>,
+    inner: Vec<TodoListGroup>,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
-pub struct KitchenTabView {
+pub struct TodoListGroup {
     pub(crate) tab_id: TabId,
-    pub(crate) food_items: Vec<KitchenTabItem>,
+    pub(crate) food_items: Vec<TodoListItem>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
-pub struct KitchenTabItem {
+pub struct TodoListItem {
     pub(crate) menu_number: usize,
     pub(crate) description: String,
 }
 
 impl KitchenTodoList {
     pub fn new() -> Self {
-        Self {
-            inner: RwLock::new(Vec::new()),
-        }
+        Self { inner: Vec::new() }
     }
 }
 
-impl KitchenTabView {
+impl TodoListGroup {
     pub fn tab_id(&self) -> TabId {
         self.tab_id
     }
 
-    pub fn food_items(&self) -> Vec<KitchenTabItem> {
+    pub fn food_items(&self) -> Vec<TodoListItem> {
         let mut result = Vec::new();
         for item in self.food_items.iter() {
             result.push((*item).clone());
@@ -50,20 +47,28 @@ impl KitchenTabView {
     }
 }
 
-impl KitchenTabItem {
+impl TodoListItem {
     pub fn new(menu_number: usize, description: &str) -> Self {
         Self {
             menu_number,
             description: description.to_owned(),
         }
     }
+
+    pub fn menu_number(&self) -> usize {
+        self.menu_number
+    }
+
+    pub fn description(&self) -> String {
+        self.description.clone()
+    }
 }
 
-impl View<Tab> for KitchenTabView {
+impl View<Tab> for TodoListGroup {
     fn update(&mut self, event: &cqrs_es::EventEnvelope<Tab>) {
         match &event.payload {
             crate::domain::tab::event::TabEvent::FoodOrderPlaced { id, menu_item } => {
-                let tab_item = KitchenTabItem {
+                let tab_item = TodoListItem {
                     menu_number: menu_item.menu_number,
                     description: menu_item.description.clone(),
                 };
@@ -79,16 +84,56 @@ impl View<Tab> for KitchenTabView {
     }
 }
 
+impl View<Tab> for KitchenTodoList {
+    fn update(&mut self, event: &cqrs_es::EventEnvelope<Tab>) {
+        match &event.payload {
+            crate::domain::tab::event::TabEvent::FoodOrderPlaced { id, menu_item } => {
+                let mut todo_group = None;
+                for group in self.inner.iter_mut() {
+                    if group.tab_id == *id {
+                        todo_group = Some(group);
+                        break;
+                    }
+                }
+                let tab_item = TodoListItem {
+                    menu_number: menu_item.menu_number,
+                    description: menu_item.description.clone(),
+                };
+                match todo_group {
+                    Some(group) => group.food_items.push(tab_item),
+                    None => self.inner.push(TodoListGroup {
+                        tab_id: *id,
+                        food_items: vec![tab_item],
+                    }),
+                };
+            }
+            crate::domain::tab::event::TabEvent::FoodPrepared {
+                id: _,
+                menu_number: _,
+            } => {}
+            _ => {}
+        }
+    }
+}
+
 #[async_trait]
 impl KitchenTodoListQuery for KitchenTodoList {
-    async fn get_kitchen_todo_list(&self) -> Vec<KitchenTabView> {
-        let inner = self.inner.read().await;
-        let mut result = Vec::with_capacity(inner.len());
-        for item in inner.iter() {
+    async fn get_kitchen_todo_list(&self) -> Vec<TodoListGroup> {
+        // let inner = self.inner.read().await;
+        let mut result = Vec::with_capacity(self.inner.len());
+        for item in self.inner.iter() {
             result.push(item.clone())
         }
 
         result
+    }
+}
+
+impl std::ops::Deref for KitchenTodoList {
+    type Target = Vec<TodoListGroup>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
     }
 }
 
--- tests/main.rs
+++ tests/main.rs
@@ -1,5 +1,10 @@
 use cafe_tab::{
-    domain::tab::{command::TabCommand, services::TabServices, tab_id::TabId, waiter_id::WaiterId},
+    domain::tab::{
+        command::{OrderItem, TabCommand},
+        services::TabServices,
+        tab_id::TabId,
+        waiter_id::WaiterId,
+    },
     infrasctructure::{
         persistence::context::{
             connection_parameters::ConnectionBuilder,
@@ -9,11 +14,12 @@ use cafe_tab::{
     },
     shared_kernel::KitchenTabViewRepository,
 };
+use rust_decimal::Decimal;
 use secrecy::Secret;
 use uuid::Uuid;
 
 #[tokio::test]
-async fn initially_tab_is_empty() {
+async fn initially_kitchen_todo_list_is_empty() {
     // Arrange
     let db_name = Uuid::new_v4().to_string();
     let params = ConnectionBuilder::new()
@@ -48,6 +54,62 @@ async fn initially_tab_is_empty() {
         .await
         .expect("failed to load the kitchen tab view")
         .unwrap();
-    assert_eq!(actual.tab_id(), id);
-    assert_eq!(actual.food_items().len(), 0);
+    assert_eq!(actual.len(), 0);
+}
+
+#[tokio::test]
+async fn given_new_tab_when_1_food_order_then_kitchen_list_view_shows_1_food_order() {
+    // Arrange
+    let db_name = Uuid::new_v4().to_string();
+    let params = ConnectionBuilder::new()
+        .with_credentials("postgres", Secret::new(String::from("password")))
+        .with_database(&db_name)
+        .build();
+    create_db(&params).await;
+    migrate_db(&params).await;
+    let pool = postgres_pool(&params).await;
+    let kitchen_tab_view_repo = KitchenTabViewRepository::new(pool.clone());
+    let services = TabServices {};
+    let tab_aggregate = cqrs_tab(pool, services, kitchen_tab_view_repo.clone());
+    let id = TabId::default();
+    let id_string = id.to_string();
+    tab_aggregate
+        .execute(
+            &id_string,
+            TabCommand::OpenTab {
+                id,
+                waiter_id: WaiterId::new(),
+                table: 1,
+            },
+        )
+        .await
+        .expect("failed to open tab");
+
+    // Act
+    tab_aggregate
+        .execute(
+            &id_string,
+            TabCommand::PlaceOrder {
+                order_items: vec![OrderItem {
+                    menu_number: 1,
+                    description: "Steak".into(),
+                    is_drink: false,
+                    price: Decimal::from(10),
+                }],
+            },
+        )
+        .await
+        .expect("failed to order food");
+
+    // Assert
+    let actual = kitchen_tab_view_repo
+        .load(&id_string)
+        .await
+        .expect("failed to load the kitchen tab view")
+        .unwrap();
+    assert_eq!(actual.len(), 1);
+    assert_eq!(actual[0].tab_id(), id);
+    assert_eq!(actual[0].food_items().len(), 1);
+    assert_eq!(actual[0].food_items()[0].menu_number(), 1);
+    assert_eq!(actual[0].food_items()[0].description(), "Steak");
 }
