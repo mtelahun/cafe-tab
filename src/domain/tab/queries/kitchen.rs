d70a528e75b384682c71416589f95f9603ddb483
Michael Telahun <michael.telahun@dindin.et>
2024-04-09 10:40:47 +0300

    test: newly created kitchen todo list is empty

--- src/domain/tab/queries/kitchen.rs
+++ src/domain/tab/queries/kitchen.rs
@@ -1,12 +1,18 @@
 use async_trait::async_trait;
 use cqrs_es::View;
 use serde::{Deserialize, Serialize};
+use tokio::sync::RwLock;
 
 use crate::domain::tab::{aggregate::Tab, tab_id::TabId};
 
 #[async_trait]
 pub trait KitchenTodoListQuery: Sized {
     async fn get_kitchen_todo_list(&self) -> Vec<KitchenTabView>;
+}
+
+#[derive(Debug, Default)]
+pub struct KitchenTodoList {
+    inner: RwLock<Vec<KitchenTabView>>,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
@@ -19,6 +25,14 @@ pub struct KitchenTabView {
 pub struct KitchenTabItem {
     pub(crate) menu_number: usize,
     pub(crate) description: String,
+}
+
+impl KitchenTodoList {
+    pub fn new() -> Self {
+        Self {
+            inner: RwLock::new(Vec::new()),
+        }
+    }
 }
 
 impl KitchenTabView {
@@ -66,8 +80,23 @@ impl View<Tab> for KitchenTabView {
 }
 
 #[async_trait]
-impl KitchenTodoListQuery for KitchenTabView {
+impl KitchenTodoListQuery for KitchenTodoList {
     async fn get_kitchen_todo_list(&self) -> Vec<KitchenTabView> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::tab::queries::kitchen::KitchenTodoListQuery;
+
+    use super::KitchenTodoList;
+
+    #[tokio::test]
+    #[allow(non_snake_case)]
+    async fn given_new_KitchenTodoList_then_it_is_empty() {
+        let list = KitchenTodoList::new();
+
+        assert!(list.get_kitchen_todo_list().await.is_empty())
     }
 }
