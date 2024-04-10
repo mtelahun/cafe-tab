2bb4567a6ce85005c4c76b7670960e51bda06e2c
Michael Telahun <michael.telahun@dindin.et>
2024-04-10 14:09:50 +0300

    test: an item added to an open tab can be retrieved by calling open_items() on the open tab

--- src/domain/tab/queries/open_tabs.rs
+++ src/domain/tab/queries/open_tabs.rs
@@ -12,18 +12,18 @@ pub struct OpenTab {
     open_items: Vec<OpenItem>,
 }
 
-// impl OpenTab {
-//     pub fn new(id: TabId) -> Self {
-//         Self {
-//             id,
-//             open_items: Vec::new(),
-//         }
-//     }
-// }
+impl OpenTab {
+    pub fn new(id: TabId) -> Self {
+        Self {
+            id,
+            open_items: Vec::new(),
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
-    use crate::domain::tab::tab_id::TabId;
+    use crate::domain::tab::{queries::open_tabs::OpenItem, tab_id::TabId};
 
     use super::OpenTab;
 
@@ -32,5 +32,24 @@ mod test {
         let tab = OpenTab::new(TabId::default());
 
         assert!(tab.open_items.is_empty())
+    }
+
+    #[test]
+    fn given_open_tab_when_add_open_item_then_open_items_has_one_item() {
+        // Arrange
+        let id = TabId::new();
+        let tab = OpenTab::new(id);
+        let item = OpenItem {
+            menu_number: 2,
+            description: "Coca-Cola".into(),
+        };
+
+        // Act
+        tab.add_item(id, item);
+
+        assert_eq!(tab.id(), id);
+        assert_eq!(tab.open_items.len(), 1);
+        assert_eq!(tab.open_items()[0].menu_number(), 2);
+        assert_eq!(tab.open_items()[0].description(), "Coca-Cola");
     }
 }
